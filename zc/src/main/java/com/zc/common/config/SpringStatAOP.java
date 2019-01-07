2863837b68f20f9def334f0215b8e1d32f97e011
zichen1019 <1510748736@qq.com>
2019-01-07 17:28:23 +0800

    Druid配置SQL、Spring等监控信息 并且设置可视化界面的登录及访问信息等 由于目前不知道如何在yml中配置druid的Spring Stat监控,所以暂时使用SpringStatAOP暂代.

---
+++ zc/src/main/java/com/zc/common/config/SpringStatAOP.java
@@ -0,0 +1,40 @@
+package com.zc.common.config;
+
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * 由于目前不知道如何在yml中配置druid的Spring Stat监控
+ * 所以暂时使用当前类暂代.
+ */
+@Configuration
+public class SpringStatAOP {
+
+    @Bean
+    public DruidStatInterceptor druidStatInterceptor() {
+        DruidStatInterceptor dsInterceptor = new DruidStatInterceptor();
+        return dsInterceptor;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public JdkRegexpMethodPointcut druidStatPointcut() {
+        JdkRegexpMethodPointcut pointcut = new JdkRegexpMethodPointcut();
+        // 监控的类所在的包名，多个包名用 英文逗号 隔开.
+        pointcut.setPattern("com.zc.controller.*,com.zc.service.*,com.zc.mapper.*.*");
+        return pointcut;
+    }
+
+    @Bean
+    public DefaultPointcutAdvisor druidStatAdvisor(DruidStatInterceptor druidStatInterceptor, JdkRegexpMethodPointcut druidStatPointcut) {
+        DefaultPointcutAdvisor defaultPointAdvisor = new DefaultPointcutAdvisor();
+        defaultPointAdvisor.setPointcut(druidStatPointcut);
+        defaultPointAdvisor.setAdvice(druidStatInterceptor);
+        return defaultPointAdvisor;
+    }
+
+}
