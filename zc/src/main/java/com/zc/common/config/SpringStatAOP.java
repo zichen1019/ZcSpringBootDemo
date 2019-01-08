5456ffb6973938dd355b3e2d04240d27cbda2ae6
zichen1019 <1510748736@qq.com>
2019-01-08 10:38:21 +0800

    Druid的Spring监控无数据： 发现拦截类中的patterns设置无效 此处需要配合AOP使用，所以在yml中配置以下属性： spring:   # 此处AOP配置，是druid的Spring监控所需要   aop:     auto: true     proxy-target-class: true   datasource:       druid:         # Spring监控AOP切入点，如x.y.z.service.*,配置多个英文逗号分隔         aop-patterns: com.zc.controller.*,com.zc.service.*,com.zc.service.*.*,com.zc.mapper.*.*

--- zc/src/main/java/com/zc/common/config/SpringStatAOP.java
+++ zc/src/main/java/com/zc/common/config/SpringStatAOP.java
@@ -24,8 +24,23 @@ public class SpringStatAOP {
     @Scope("prototype")
     public JdkRegexpMethodPointcut druidStatPointcut() {
         JdkRegexpMethodPointcut pointcut = new JdkRegexpMethodPointcut();
-        // 监控的类所在的包名，多个包名用 英文逗号 隔开.
-        pointcut.setPattern("com.zc.controller.*,com.zc.service.*,com.zc.mapper.*.*");
+        /**
+         * // 不知为何此处代码不生效
+         * // 监控的类所在的包名，多个包名用 英文逗号 隔开.
+         * // pointcut.setPattern("com.zc.controller.*");
+         * // pointcut.setPatterns("com.zc.controller.*,com.zc.service.*,com.zc.service.*.*,com.zc.mapper.*.*");
+         */
+        /**
+         * # 在yml配置以下属性与当前类进行搭配使用
+         * spring:
+         *   aop:
+         *     auto: true
+         *     proxy-target-class: true
+         *   datasource:
+         *     druid:
+         *       # Spring监控AOP切入点，如x.y.z.service.*,配置多个英文逗号分隔
+         *       aop-patterns: com.zc.controller.*,com.zc.service.*,com.zc.service.*.*,com.zc.mapper.*.*
+         */
         return pointcut;
     }
 
